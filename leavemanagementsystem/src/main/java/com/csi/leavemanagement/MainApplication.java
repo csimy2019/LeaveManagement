171d899c5ca3de88d4dfb443fe79562a821d78ea
shahrul91 <shahrul91@gmail.com>
2019-01-02 17:10:44 +0800

    Removed TODO comment. Jame

--- leavemanagementsystem/src/main/java/com/csi/leavemanagement/MainApplication.java
+++ leavemanagementsystem/src/main/java/com/csi/leavemanagement/MainApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MainApplication {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		SpringApplication.run(MainApplication.class, args);
+	public static void main(String[] args) {		
+		SpringApplication.run(MainApplication.class, args);	
 	}
-
 }
